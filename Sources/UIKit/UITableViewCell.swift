9cde223f8b1d4fd17070e76d765a1298f6c1ae12
Ivan <ivan.cantarino@icloud.com>
2022-01-15 23:36:57 +0000

    Add UITableViewCell

--- Sources/UIKit/UITableViewCell.swift
+++ Sources/UIKit/UITableViewCell.swift
@@ -1,8 +1,17 @@
 //
-//  File.swift
+//  UITableViewCell.swift
 //  
 //
 //  Created by Ivan Cantarino on 15/01/2022.
 //
 
-import Foundation
+import UIKit
+
+extension UITableViewCell {
+    /// Shows or hides the bottom line separator
+    /// of the cell.
+    /// - Parameter hide: boolean to either show or hide the separator.
+    public func separator(hide: Bool) {
+        separatorInset.left += hide ? bounds.size.width : 0
+    }
+}
