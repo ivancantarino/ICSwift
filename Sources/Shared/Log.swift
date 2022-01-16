87b9879c70025a7c711b2862bc21f43ea80cd24c
Ivan <ivan.cantarino@icloud.com>
2022-01-16 00:06:35 +0000

    Added Log

--- Sources/Shared/Log.swift
+++ Sources/Shared/Log.swift
@@ -1,8 +1,33 @@
 //
-//  File.swift
+//  Log.swift
 //  
 //
 //  Created by Ivan Cantarino on 15/01/2022.
 //
 
-import Foundation
+/// A logging function that only compiles while debugging, avoiding
+/// logs in the release.
+public func Log(_ input: Any = "",
+                file: String = #file,
+                function: String = #function,
+                line: Int = #line,
+                alertType: (AlertType)? = nil) {
+#if DEBUG
+    guard let alert = alertType else {
+        print("\n☞ \(input)")
+        return
+    }
+    print("\n\(file):\n\(function) Line \(line)\n\(alert.rawValue)\(input)")
+#endif
+}
+
+/// Enum that adds and emoji in the beggining of the
+/// ICLog string.
+/// - `.alert`: "⚠️"
+/// - `.error`: "❌ "
+/// - `.success`: "✅"
+public enum AlertType: String {
+    case alert = "⚠️ "
+    case error = "❌ "
+    case success = "✅ "
+}
