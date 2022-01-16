306a9768af6cea127275a3ff40421f7d270d3922
Ivan <ivan.cantarino@icloud.com>
2022-01-16 10:37:01 +0000

    Added NotificationCenter

--- Sources/Foundation/NotificationCenter.swift
+++ Sources/Foundation/NotificationCenter.swift
@@ -1,8 +1,20 @@
 //
-//  File.swift
+//  NotificationCenter.swift
 //  
 //
 //  Created by Ivan Cantarino on 16/01/2022.
 //
 
 import Foundation
+
+/// Quality-of-life extension of NotificationCenter.
+extension NotificationCenter {
+    /// Post a Notification using the `default` center
+    /// - Parameters:
+    ///   - name: Notification name identifier
+    ///   - object: Object to be handled within the notitication
+    ///   - userInfo: Aditional information to pass within the notification
+    public static func post(_ name: Notification.Name, object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) {
+            NotificationCenter.default.post(name: name, object: object, userInfo: userInfo)
+    }
+}
