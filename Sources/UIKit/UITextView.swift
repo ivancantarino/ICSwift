132dbf0c024ba8f5bf936bef8d06269217f74902
Ivan <ivan.cantarino@icloud.com>
2022-01-16 00:06:56 +0000

    Added UITextView

--- Sources/UIKit/UITextView.swift
+++ Sources/UIKit/UITextView.swift
@@ -10,7 +10,8 @@ import UIKit
 extension UITextView {
     /// Centers the text vertically
     public func centerVertically() {
-        let fittingSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let fittingSize = CGSize(width: bounds.width,
+                                 height: CGFloat.greatestFiniteMagnitude)
         let size = sizeThatFits(fittingSize)
         let topOffset = (bounds.size.height - size.height * zoomScale) / 2
         let positiveTopOffset = max(1, topOffset)
