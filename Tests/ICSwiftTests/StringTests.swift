7497afe6c432f06d6ee0548683c49cb93281ce7d
Ivan <ivan.cantarino@icloud.com>
2022-01-16 16:37:19 +0000

    Create String Unit Tests

--- Tests/ICSwiftTests/StringTests.swift
+++ Tests/ICSwiftTests/StringTests.swift
@@ -1,8 +1,31 @@
 //
-//  File.swift
+//  StringTests.swift
 //  
 //
 //  Created by Ivan Cantarino on 16/01/2022.
 //
 
-import Foundation
+import XCTest
+@testable import ICSwift
+
+
+/// UnitTests for the each `String` extension.
+final class StringTests: XCTestCase {
+    private var sut: String = "hello world!"
+    
+    override func tearDown() {
+        sut = "hello world!"
+    }
+    
+    func testFirstLetterCapitalized() {
+        XCTAssertEqual(sut.capitalizingFirstLetter(), "Hello world!")
+    }
+    
+    func testSubscriptForCharAtIndex() {
+        XCTAssertEqual(sut[4], "o")
+    }
+    
+    func testSubscriptForRange() {
+        XCTAssertEqual(sut[2..<7], "llo w")
+    }
+}
