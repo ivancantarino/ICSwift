1c6d08b375f9579062e61ca1d2c8b561571e6443
Ivan <ivan.cantarino@icloud.com>
2022-01-16 16:50:35 +0000

    Added Int Unit Tests

--- Tests/ICSwiftTests/IntTests.swift
+++ Tests/ICSwiftTests/IntTests.swift
@@ -1,8 +1,16 @@
 //
-//  File.swift
+//  IntTests.swift
 //  
 //
 //  Created by Ivan Cantarino on 16/01/2022.
 //
 
-import Foundation
+import XCTest
+@testable import ICSwift
+
+final class IntTests: XCTestCase {
+    func testDegreesToRadians() {
+        let i = 180
+        XCTAssertEqual(i.degreesToRadians, Double.pi)
+    }
+}
